6df434d9d0bf4c280476e4e74c9f4a6737b9b931
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2016-08-02 22:11:44 +0530

    add basic code

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type User struct {
+	ID       bson.ObjectId `bson:"_id,omitempty"`
+	Name     string
+	Username string
+	provider string
+}
+
+func main() {
+	var mongodbURL = os.Getenv("MONGODB_URL")
+	session, err := mgo.Dial(mongodbURL)
+	if err != nil {
+		panic(err)
+	}
+	//session.SetMode(mgo.Monotonic, true)
+	var results []User
+	c := session.DB("ntwitter").C("users")
+	err = c.Find(bson.M{"provider": "github"}).All(&results)
+	if err != nil {
+		panic(err)
+	}
+	// fmt.Println("username", result.Username)
+	for _, result := range results {
+		fmt.Println("username", result.Username)
+	}
+	fmt.Println("username", results)
+
+}
+}
