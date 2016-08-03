6ce33ae7ec6c29f5cc8c6ff7190729610cb13e98
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2016-08-03 13:16:40 +0530

    add query for tweets.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	mgo "gopkg.in/mgo.v2"
@@ -21,10 +22,16 @@ type User struct {
 type Tweet struct {
 	ID   bson.ObjectId `bson:"_id,omitempty"`
 	Body string
-	User bson.ObjectId `bson:"_id,omitempty"`
+	User bson.ObjectId
 }
 
 func connectDB(dailURL string) (*mgo.Session, error) {
+	mgo.SetDebug(true)
+
+	var aLogger *log.Logger
+	aLogger = log.New(os.Stderr, "", log.LstdFlags)
+	mgo.SetLogger(aLogger)
+
 	session, err := mgo.Dial(dailURL)
 	fmt.Println("Trying to connect to DB")
 	if err != nil {
@@ -35,26 +42,48 @@ func connectDB(dailURL string) (*mgo.Session, error) {
 }
 
 func getUsers(session *mgo.Session, err error) {
-
 	c := session.DB("ntwitter").C("users")
 	var userResult User
 	fmt.Println("Trying to query one users")
 	err = c.Find(bson.M{"username": "vinitkumar"}).One(&userResult)
 	fmt.Println("user query", userResult)
+	fmt.Print("userID", userResult.ID)
+
 	var results []User
 	fmt.Println("Trying to find all users of a certain provider")
 	err = c.Find(bson.M{"provider": "github"}).All(&results)
 	if err != nil {
 		panic(err)
 	}
-	for _, result := range results {
-		fmt.Println("username", result.Username)
-	}
-	fmt.Println("username", results)
+	//for _, result := range results {
+	//	fmt.Println("username", result.Username, result.Provider)
+	//}
+	// fmt.Println("username", results)
+}
+
+func getTweets(session *mgo.Session, err error) {
+	connectionUser := session.DB("ntwitter").C("users")
+	var userResult User
+	err = connectionUser.Find(bson.M{"username": "vinitkumar"}).One(&userResult)
+	fmt.Println("Finding user for a tweet query")
+	fmt.Print("userID", userResult.ID)
+
+	connectionTweet := session.DB("ntwitter").C("tweets")
+	var tweet Tweet
+	err = connectionTweet.Find(bson.M{"user": userResult.ID}).One(&tweet)
+	fmt.Println("tweet", tweet)
+
+	err = connectionTweet.Find(bson.M{"Body": "Kya chutiyaapa hai"}).One(&tweet)
+	fmt.Println("tweet", tweet)
+	var tweets []Tweet
+	fmt.Println("sample request", bson.M{"user": userResult.ID})
+	err = connectionTweet.Find(bson.M{"user": userResult.ID}).All(&tweets)
+	fmt.Println("tweets", tweets)
 }
 
 func main() {
 	var mongodbURL = os.Getenv("MONGODB_URL")
 	session, err := connectDB(mongodbURL)
 	getUsers(session, err)
+	getTweets(session, err)
 }
